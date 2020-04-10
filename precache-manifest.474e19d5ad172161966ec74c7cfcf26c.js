b47548acd24044299ca0d25e3bcb0f88fd055472
Jorge Jimenez <jorgegom-c@herbalife.com>
2020-04-09 19:34:13 -0500

    Updates

---
+++ precache-manifest.474e19d5ad172161966ec74c7cfcf26c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de72a1e245a977d7bb487ae7ac49a15f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dd6170e4e831423247a1",
+    "url": "/monsters-rolodex/static/css/main.c3196797.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6170e4e831423247a1",
+    "url": "/monsters-rolodex/static/js/main.f242ef9d.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
